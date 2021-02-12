185b927cd85171a26e61a423ed2d01ec689d75c5
1059Mustafa <72117500+1059Mustafa@users.noreply.github.com>
2021-02-12 15:20:21 +0530

    Add files via upload

---
+++ binary_6.cpp
@@ -0,0 +1,35 @@
+vector<int> inOrder(Node* root)
+{
+    vector<int>v;
+    stack<Node *> s;
+    Node *curr = root;
+ 
+    while (curr != NULL || s.empty() == false)
+    {
+        /* Reach the left most Node of the
+           curr Node */
+        while (curr !=  NULL)
+        {
+            /* place pointer to a tree node on
+               the stack before traversing
+              the node's left subtree */
+            s.push(curr);
+            curr = curr->left;
+        }
+ 
+        /* Current must be NULL at this point */
+        curr = s.top();
+        s.pop();
+ 
+        v.push_back(curr->data);
+ 
+        /* we have visited the node and its
+           left subtree.  Now, it's right
+           subtree's turn */
+        curr = curr->right;
+ 
+    } /* end of while */
+
+    return v;
+    
+}
